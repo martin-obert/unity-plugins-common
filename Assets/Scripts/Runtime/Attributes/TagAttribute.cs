057acd916dc7e922914f1788ac4ec7166135dc56
Martin Obert <martin@obert.cz>
2023-01-22 20:10:26 +0100

    feat: added some editor utilities
    Mainly added Scene and Tag drawer and few GameObject extensions

---
+++ Assets/Scripts/Runtime/Attributes/TagAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+
+namespace Obert.Common.Runtime.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field)]
+    public class TagAttribute : PropertyAttribute
+    {
+        
+    }
+}
